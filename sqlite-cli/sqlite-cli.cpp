4a6c031ccc7d6b89fdf48dcb5976047b92b865ef
Ashod Nakashian <ashodnakashian@yahoo.com>
2011-09-13 09:01:51 +0500

    Test cleanup.
    Updated ignore list.

--- sqlite-cli/sqlite-cli.cpp
+++ sqlite-cli/sqlite-cli.cpp
@@ -51,10 +51,9 @@ static int callback_check(void *NotUsed, int argc, char **argv, char **azColName
 
     for(i=0; i<argc; i++)
     {
-        //printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
         if (strcmp(expected_value, argv[i]) != 0)
         {
-            printf("Error: value mismatch.\nExpected: %s\n Got: %s\n", expected_value, argv[i]);
+            printf("ERROR: value mismatch.\nExpected: %s\n Got: %s\n", expected_value, argv[i]);
             exit(1);
         }
         else
@@ -160,156 +159,6 @@ void CreateLargeDB(_TCHAR* dbFilename)
     GetSparseFileSize(dbFilename);
 }
 
-void UpdateLarge(_TCHAR* dbFilename)
-{
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc,i;
-
-    char **result;
-    int nrow;
-    int ncol;
-
-    rc = sqlite3_open(dbFilename, &db);
-    if( rc ){
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        exit(1);
-    }
-
-    const char const* CREATE_TABLE_COMMAND = "create table t1 (key INTEGER PRIMARY KEY, data TEXT);";
-    rc = sqlite3_exec(db, CREATE_TABLE_COMMAND, callback, 0, &zErrMsg);
-
-    printf(">> Insert\n");
-    rc = sqlite3_exec(db, "insert into t1 (key, data) values (1, 'This is sample data');", callback, 0, &zErrMsg);
-    rc = sqlite3_exec(db, "insert into t1 (key, data) values (2, 'Another sample data');", callback, 0, &zErrMsg);
-    rc = sqlite3_exec(db, "insert into t1 (key, data) values (3, 'Third sample data');", callback, 0, &zErrMsg);
-
-    printf(">> Update\n");
-    const char const* UPDATE_COMMAND = "update t1 set data = 'asdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadkasdfghjklkajksfkjsdkadk' where key=1;";
-    rc = sqlite3_exec(db, UPDATE_COMMAND, callback, 0, &zErrMsg);
-
-    if( rc!=SQLITE_OK ){
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-    }
-    sqlite3_close(db);
-}
-
-BOOL SparseFileSuppored(LPCTSTR lpVolRootPath)
-{
-    DWORD dwFlags;
-
-    GetVolumeInformation(
-        lpVolRootPath,
-        NULL,
-        MAX_PATH,
-        NULL,
-        NULL,
-        &dwFlags,
-        NULL,
-        MAX_PATH);
-
-    return (dwFlags & FILE_SUPPORTS_SPARSE_FILES);
-
-}
-
-HANDLE CreateSparseFile(LPCTSTR lpSparseFileName)
-{
-    // Use CreateFile as you would normally - Create file with whatever flags
-    //and File Share attributes that works for you
-    DWORD dwTemp;
-
-    HANDLE hSparseFile = CreateFile(lpSparseFileName,
-        GENERIC_READ|GENERIC_WRITE,
-        FILE_SHARE_READ|FILE_SHARE_WRITE,
-        NULL,
-        OPEN_ALWAYS,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL);
-
-    if (hSparseFile == INVALID_HANDLE_VALUE)
-        return hSparseFile;
-
-    BOOL res = DeviceIoControl(hSparseFile,
-        FSCTL_SET_SPARSE,
-        NULL,
-        0,
-        NULL,
-        0,
-        &dwTemp,
-        NULL);
-    return hSparseFile;
-}
-
-DWORD SetSparseRange(HANDLE hSparseFile, LONGLONG start, LONGLONG size)
-{
-    // Specify the starting and the ending address (not the size) of the
-    // sparse zero block
-    FILE_ZERO_DATA_INFORMATION fzdi;
-    fzdi.FileOffset.QuadPart = start;
-    fzdi.BeyondFinalZero.QuadPart = start + size;
-    // Mark the range as sparse zero block
-    DWORD dwTemp;
-    SetLastError(0);
-    BOOL bStatus = DeviceIoControl(hSparseFile,
-        FSCTL_SET_ZERO_DATA,
-        &fzdi,
-        sizeof(fzdi),
-        NULL,
-        0,
-        &dwTemp,
-        NULL);
-    if (bStatus) return 0; //Sucess
-    else
-    {
-        DWORD e = GetLastError();
-        return(e); //return the error value
-    }
-}
-
-void CreateSparse()
-{
-    HANDLE hFile = CreateSparseFile("SparseFile");
-
-    //::SetFilePointer(hFile, 0x1000000, NULL, FILE_END);
-    //::SetEndOfFile(hFile);
-
-    const int SIZE = 640000;
-    char* buffer = new char[SIZE];
-    memset(buffer, 'a', SIZE);
-
-    DWORD wrote = 0;
-    DWORD written = 0;
-    WriteFile(hFile, buffer, SIZE, &written, NULL);
-    wrote += written;
-
-    for (int i = 0; i < SIZE; ++i)
-    {
-        int start = i * 64 * 1024;
-        SetSparseRange(hFile, start + 1, (64 * 1024) - 1);
-    }
-
-    printf("Wrote %d bytes.\n", wrote);
-    CloseHandle(hFile);
-}
-
-void ReadSparse()
-{
-    HANDLE hFile = CreateSparseFile("SparseFile");
-
-    const int SIZE = 6 * 6 * 512;
-    char* buffer = new char[SIZE];
-    memset(buffer, 0, SIZE);
-
-    DWORD read = 0;
-    ReadFile(hFile, buffer, SIZE, &read, NULL);
-    printf("Read %d bytes.\n", read);
-
-    printf(buffer);
-
-    CloseHandle(hFile);
-}
-
 BOOL GetSparseFileSize(LPCTSTR lpFileName)
 {
     // Retrieves the size of the specified file, in bytes. The size includes
@@ -370,6 +219,29 @@ void QueryWikideskDB(_TCHAR* dbFilename)
     sqlite3_close(db);
 }
 
+int QuickTest(LPCTSTR lpFilename, char* query)
+{
+    sqlite3 *db;
+    //DeleteFile(lpFilename);
+
+    vfscompress_register(0, -1);
+    int rc = sqlite3_open(lpFilename, &db);
+
+    if (rc == SQLITE_OK)
+    {
+        char *zErrMsg = 0;
+        rc = sqlite3_exec(db, query, callback, 0, &zErrMsg);
+    }
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Sqlite Error: %s\n", sqlite3_errmsg(db));
+    }
+
+    sqlite3_close(db);
+    return rc;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     sqlite3 *db;
@@ -380,24 +252,28 @@ int _tmain(int argc, _TCHAR* argv[])
     int nrow;
     int ncol;
 
+    QuickTest("Z:\\test.db", 
+        "CREATE TABLE abc(a PRIMARY KEY, b, c);\
+        INSERT INTO abc VALUES(1, 2, 3);\
+        INSERT INTO abc VALUES(2, 3, 4);\
+        INSERT INTO abc SELECT a+2, b, c FROM abc;\
+        SELECT * FROM abc;");
+
+    QuickTest("Z:\\test.db", 
+        "CREATE TABLE abc(a PRIMARY KEY, b, c);\
+        INSERT OR REPLACE INTO abc VALUES(1, 2, 3);\
+        INSERT OR REPLACE INTO abc VALUES(1, 2, 4);\
+        INSERT OR REPLACE INTO abc SELECT a+2, b, c FROM abc;\
+        SELECT * FROM abc;");
+
     //QueryWikideskDB("Z:\\wikidesk.db");
-    CreateLargeDB("Z:\\test.db");
-    return 0;
-
-
-    printf("Supported: %s\n", SparseFileSuppored("C:\\") ? "Yes" : "No");
-
-    CreateSparse();
-    GetSparseFileSize("SparseFile");
-    ReadSparse();
+    //CreateLargeDB("Z:\\test.db");
     return 0;
 
     if( argc!=3 ){
         fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
         exit(1);
     }
-
-    UpdateLarge(argv[1]);
 
     rc = sqlite3_open(argv[1], &db);
     if( rc ){
@@ -420,14 +296,12 @@ int _tmain(int argc, _TCHAR* argv[])
     for(i=0 ; i < nrow+ncol; ++i)
         printf("%s ",result[i]);
 
-
-
    sqlite3_free_table(result);
 
-    if( rc!=SQLITE_OK ){
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-    }
-    sqlite3_close(db);
+   if( rc!=SQLITE_OK ){
+       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+   }
+   sqlite3_close(db);
     return 0;
 }
 
