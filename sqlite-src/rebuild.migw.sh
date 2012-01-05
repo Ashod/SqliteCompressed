fb65aed093d79e88eab5c62cceb6cc100cecdf1b
Ashod <ashodnakashian@yahoo.com>
2012-01-05 20:58:30 +0400

    Upgraded MinGW from 4.5.2 to 4.6.1 and added the source folder to the linker path to find zlib.

--- sqlite-src/rebuild.migw.sh
+++ sqlite-src/rebuild.migw.sh
@@ -3,9 +3,13 @@
 # This script is used to configure and build SQLite on MingW.
 #
 
-TCLDIR=/c/tools/Tcl/lib/tcl8.5
+TCLDIR=/c/Tools/Tcl/lib/tcl8.5
 
-./configure --with-tcl=$TCLDIR
+make distclean
 
-make rebuild
-make build_tests
+./configure --with-tcl=$TCLDIR LDFLAGS=-L.
+
+make clean
+make all
+make dll
+make compresstest
